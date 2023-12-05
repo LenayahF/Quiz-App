91f40c40fa0a2cc09a0f698254ae6b19fb5ffa5e
Lenayah <lfanesie@gmail.com>
2023-12-05 14:18:31 -0500

    update

---
+++ Quiz App/Quiz.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz.swift
+//  Quiz App
+//
+//  Created by 11k on 12/4/23.
+//
+
+import Foundation
+
+struct Quiz{
+    var question: String
+    var answer1: String
+    var answer2: String
+    var answer3: String
+    var answer4: String
+    var correct: String
+}
--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var checkAnswer: UIButton!
+    
+    @IBOutlet weak var d: UIButton!
+    @IBOutlet weak var c: UIButton!
+    @IBOutlet weak var b: UIButton!
+    @IBOutlet weak var a: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    var score = 0
+    var index = 0
+    var quiz:[Quiz] = [Quiz(question: "Question", answer1: "1", answer2: "3", answer3: "2", answer4: "4", correct: "4"), ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scoreLabel.text = String(score)
+        questionLabel.text = quiz[index].question
+        a.setTitle(quiz[index].answer1, for: .normal)
+        b.setTitle(quiz[index].answer2, for: .normal)
+        c.setTitle(quiz[index].answer3, for: .normal)
+       d.setTitle(quiz[index].answer4, for: .normal)
+        
     }
 
 
